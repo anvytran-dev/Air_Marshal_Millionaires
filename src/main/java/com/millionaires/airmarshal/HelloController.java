5b881092f7676d8c608197dc6f81826cb900936d
Anvy Tran <78188385+anvytran-dev@users.noreply.github.com>
2022-02-07 18:15:24 -0500

    Initial commit

---
+++ src/main/java/com/millionaires/airmarshal/HelloController.java
@@ -0,0 +1,14 @@
+package com.millionaires.airmarshal;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
