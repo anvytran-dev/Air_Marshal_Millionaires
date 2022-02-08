442342363dd80603313227983fbea1476b596931
Anvy Tran <78188385+anvytran-dev@users.noreply.github.com>
2022-02-08 12:49:19 -0500

    added json in maven

---
+++ src/main/java/com/millionaires/airmarshal/client/Main.java
@@ -0,0 +1,11 @@
+package com.millionaires.airmarshal.client;
+
+import com.nimble_four.AirMarshal.controller.Game;
+
+class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.execute();
+
+    }
+}
