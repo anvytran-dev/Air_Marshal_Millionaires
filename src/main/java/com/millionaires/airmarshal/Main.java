91b6f78b8162f6b4a0307e1283383b7f23d1d047
Anvy Tran <78188385+anvytran-dev@users.noreply.github.com>
2022-02-09 13:01:29 -0500

    adding resource folder
    added resource folder

--- src/main/java/com/millionaires/airmarshal/Main.java
+++ src/main/java/com/millionaires/airmarshal/Main.java
@@ -1,8 +1,9 @@
 package com.millionaires.airmarshal;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Scene scene = new Scene(new VBox(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
